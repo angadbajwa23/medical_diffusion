1deda30b89c4dc73e6c795d974bbd7487a9b5707
aditya.kumar <aditya.kumar@edf-inc.com>
2024-04-15 00:49:53 -0400

    data sampler update

--- data_selector.py
+++ data_selector.py
@@ -27,7 +27,7 @@ def copy_images(parent_dir, train=True):
             images = images[num_train_samples+1 :num_val_samples]
         for image_name in images:
             source_path = os.path.join('ham/images', image_name+".JPG")  # Assuming images are located in 'data' folder
-            destination_path = os.path.join(label_dir, image_name+".JPG")
+            destination_path = os.path.join(label_dir, image_name+".jpg")
             shutil.copy(source_path, destination_path)
 
 copy_images("train")
@@ -39,29 +39,29 @@ copy_images("val", train=False)
 ############## Not Used ##############
 ## Without class folders directly into train and val
 
-def copy_data(output_folder, samples):
-    os.makedirs(output_folder, exist_ok=True)
-    for image_name in samples:
-        source_path = os.path.join('ham/images', image_name + '.jpg')  # Assuming image files have extension '.jpg'
-        destination_path = os.path.join(output_folder, image_name + '.jpg')
-        shutil.copy(source_path, destination_path)  # Copy the image to the new folder
+# def copy_data(output_folder, samples):
+#     os.makedirs(output_folder, exist_ok=True)
+#     for image_name in samples:
+#         source_path = os.path.join('ham/images', image_name + '.jpg')  # Assuming image files have extension '.jpg'
+#         destination_path = os.path.join(output_folder, image_name + '.jpg')
+#         shutil.copy(source_path, destination_path)  # Copy the image to the new folder
 
-train_samples, val_samples = [], []
-for c in class_to_img:
-    train_samples.extend(class_to_img[c][:num_train_samples])
-    val_samples.extend(class_to_img[c][num_train_samples+1: num_train_samples+num_val_samples])
+# train_samples, val_samples = [], []
+# for c in class_to_img:
+#     train_samples.extend(class_to_img[c][:num_train_samples])
+#     val_samples.extend(class_to_img[c][num_train_samples+1: num_train_samples+num_val_samples])
 
-# Shuffle the final list
-print(train_samples[1],train_samples[55], train_samples[-1],"\n")
+# # Shuffle the final list
+# print(train_samples[1],train_samples[55], train_samples[-1],"\n")
 
-random.shuffle(train_samples)
-random.shuffle(val_samples)
+# random.shuffle(train_samples)
+# random.shuffle(val_samples)
 
-print(train_samples[1],train_samples[55], train_samples[-1])
-print(val_samples[1],val_samples[5], val_samples[-1])
+# print(train_samples[1],train_samples[55], train_samples[-1])
+# print(val_samples[1],val_samples[5], val_samples[-1])
 
-# ISIC_0027719 ISIC_0024994 ISIC_0031861
-# ISIC_0027488 ISIC_0028644 ISIC_0026044
+# # ISIC_0027719 ISIC_0024994 ISIC_0031861
+# # ISIC_0027488 ISIC_0028644 ISIC_0026044
 
-copy_data("val",val_samples)
-copy_data("train",train_samples)
+# copy_data("val",val_samples)
+# copy_data("train",train_samples)
